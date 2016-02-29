a5cbc648c10c75ee214f8562043ad2f5591b1645
Pavel <pps@rutp.ru>
2016-03-01 04:47:50 +0600

    Composer.lock update

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -8,10 +8,35 @@
  */
 
 
-namespace App{
+namespace App\Models{
 /**
- * App\User
+ * App\Models\Book
  *
+ * @property-read \App\Models\Author $author
+ */
+	class Book {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Author
+ *
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Book[] $books
+ */
+	class Author {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\User
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property string $remember_token
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
  */
 	class User {}
 }
